170a7d9f52061e50864745a484264ccfde7b5a28
achenlv <48532727+achenlv@users.noreply.github.com>
2022-09-16 14:28:29 +0300

    Create ap08426_a.cpp
    init a33

---
+++ ap08426_a.cpp
@@ -0,0 +1,15 @@
+/*******************************************
+Agris Pudāns, ap08426
+A33. Doti naturāli skaitļi m un n. Noteikt, vai skaitlis m ir skaitļa n vesela pakāpe. Ja ir, izdrukāt pakāpi
+Programma izveidota:  mmddyyyy
+*******************************************/
+
+
+
+
+
+/*******************************************
+Testa piemēri:
+
+
+*******************************************/
