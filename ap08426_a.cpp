c6085e797316ba84a4a17a70ca328feb9704066b
achenlv <48532727+achenlv@users.noreply.github.com>
2022-09-16 15:16:58 +0300

    test

--- ap08426_a.cpp
+++ ap08426_a.cpp
@@ -3,8 +3,12 @@ Agris Pudāns, ap08426
 A33. Doti naturāli skaitļi m un n. Noteikt, vai skaitlis m ir skaitļa n vesela pakāpe. Ja ir, izdrukāt pakāpi
 Programma izveidota:  mmddyyyy
 *******************************************/
+#import <iostream>
+#import <std>
 
-
+int main() {
+  cout >> "yeehaa" >> eoln;  
+}
 
 
 
